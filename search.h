c57cacfd7389c5942d288567cad2565ebe43a76d
pinchyCZN <mail2@crazynation.org>
2013-06-26 06:37:00 -0500

    updates

--- search.h
+++ search.h
@@ -16,10 +16,17 @@ LRESULT CALLBACK search_proc(HWND hwnd,UINT msg,WPARAM wparam,LPARAM lparam)
 	switch(msg){
 	case WM_INITDIALOG:
 		{
+			RECT rect={0};
 			if(lparam==0)
 				EndDialog(hwnd,-1);
 			win=lparam;
-			last_row=last_col=0;
+			last_row=ListView_GetSelectionMark(win->hlistview);
+			if(last_row<0)
+				last_row=0;
+			last_col=win->selected_column;
+			GetWindowRect(win->hwnd,&rect);
+			SetWindowPos(hwnd,NULL,rect.left,rect.top,0,0,SWP_NOSIZE|SWP_NOZORDER);
+			SetFocus(GetDlgItem(hwnd,IDC_EDIT1));
 		}
 		break;
 	case WM_COMMAND:
@@ -32,6 +39,41 @@ LRESULT CALLBACK search_proc(HWND hwnd,UINT msg,WPARAM wparam,LPARAM lparam)
 
 		case IDC_SEARCH_UP:
 		case IDC_SEARCH_DOWN:
+			{
+				//int dir=1;
+				int i,max,found=FALSE;
+				char find[80]={0};
+				GetWindowText(GetDlgItem(hwnd,IDC_EDIT1),find,sizeof(find));
+				max=ListView_GetItemCount(win->hlistview);
+				for(i=last_row;i<max;i++){
+					int j=last_col+1;
+					for( ;j<win->columns;j++){
+						char str[80]={0};
+						ListView_GetItemText(win->hlistview,i,j,str,sizeof(str));
+						if(strstri(str,find)!=0){
+							found=TRUE;
+							break;
+						}
+					}
+					if(found){
+						RECT rect={0};
+						last_col=j;
+						last_row=i;
+						win->selected_column=j;
+						ListView_EnsureVisible(win->hlistview,i,TRUE);
+						ListView_SetSelectionMark(win->hlistview,i);
+						ListView_GetItemRect(win->hlistview,i,&rect,LVIR_BOUNDS);
+						MapWindowPoints(win->hlistview,NULL,&rect,2);
+						printf("found at %i %i\n",last_row,last_col);
+						printf("rect %i %i\n",rect.left,rect.top);
+						SetWindowPos(hwnd,NULL,rect.left,rect.bottom,0,0,SWP_NOSIZE|SWP_NOZORDER);
+						break;
+					}
+					else
+						last_col=0;
+				}
+
+			}
 			break;
 		case IDCANCEL:
 			EndDialog(hwnd,0);
