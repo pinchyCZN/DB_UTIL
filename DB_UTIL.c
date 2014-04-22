06f9cd46e6ced51b01355026c5a37d09df568b49
pinchyCZN <mail2@crazynation.org>
2014-04-22 15:43:17 -0500

    updated defines

--- DB_UTIL.c
+++ DB_UTIL.c
@@ -1,10 +1,7 @@
-#if _WIN32_WINNT<0x400
-#define _WIN32_WINNT 0x400
-#define COMPILE_MULTIMON_STUBS
-#endif
+#define WINVER 0x500
+#define _WIN32_WINNT 0x500
 #include <windows.h>
 #include <Commctrl.h>
-#include <multimon.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <fcntl.h>
