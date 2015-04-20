4fa5e02d6c21e9fd7d8dd7bd48e62056959040ac
pinchyCZN <pinchyCZN@gmail.com>
2015-04-20 18:59:37 -0500

    adding version info

--- copy_program.bat
+++ copy_program.bat
@@ -1,6 +1,11 @@
 upx ".\release\DB_UTIL.exe"
-if not exist "C:\Program Files\DB_UTIL\DB_UTIL.exe" goto exit
+if not exist "C:\Program Files\DB_UTIL\DB_UTIL.exe" goto check64
 copy ".\release\DB_UTIL.exe" "C:\Program Files\DB_UTIL\DB_UTIL.exe"
+
+:check64
+if not exist "C:\Program Files (x86)\DB_UTIL\DB_UTIL.exe" goto exit
+copy ".\release\DB_UTIL.exe" "C:\Program Files (x86)\DB_UTIL\DB_UTIL.exe"
+
 :exit
 
 pause
